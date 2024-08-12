db12fb1084ff2850a1845bb5b5aa84de061c03e8
SahilDoshih03 <sahildsh03@gmail.com>
2024-08-12 15:55:14 +0530

    Login page created

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,26 +1,30 @@
-'use client'
+"use client";
+
+import { useRouter } from "next/navigation";
 
 interface LoginButtonProps {
-  children : React.ReactNode;
-  mode?: "modal" | "redirect"
-  asChild?: boolean
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
 }
 
-export const LoginButton = ({children, mode = "redirect", asChild}: LoginButtonProps) => {
-
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const router = useRouter();
   const onClick = () => {
-    console.log("LOGIN BUTTON CLICKED")
-  }
+    router.push("/auth/login");
+  };
 
-  if(mode==="modal"){
-    return(
-      <span>TODO: Implement Modal</span>
-    )
+  if (mode === "modal") {
+    return <span>TODO: Implement Modal</span>;
   }
 
   return (
     <span onClick={onClick} className="cursor-pointer">
       {children}
     </span>
-  )
-}
+  );
+};
